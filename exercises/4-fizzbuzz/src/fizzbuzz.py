96e300cb1542790d3f74ce1d1665e7562234b007
dstee <s.mass@gmail.com>
2025-07-28 15:00:46 +0200

    Fizzbuzz 1.3

--- exercises/4-fizzbuzz/src/fizzbuzz.py
+++ exercises/4-fizzbuzz/src/fizzbuzz.py
@@ -1,4 +1,9 @@
 def fizzbuzz(number: int) -> str:
-    if number % 3:
-        print("Fizz")
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+
     return str(number)
--- exercises/4-fizzbuzz/test_fizzbuzz.py
+++ exercises/4-fizzbuzz/test_fizzbuzz.py
@@ -1,6 +1,6 @@
 from src import fizzbuzz
 
-def fizzbuzz():
-assert fizzbuzz(1)
-assert fizzbuzz(15)
-assert fizzbuzz(9)
+def fizzbuzz(number: int) -> str:
+assert fizzbuzz(1) == "1"
+assert fizzbuzz(15) == "FizzBuzz"
+assert fizzbuzz(9) == "Fizz"
