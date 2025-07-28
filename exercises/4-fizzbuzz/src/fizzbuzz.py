66f6c17fe2f11f74a4497aff723ca3fdc1b66aa0
dstee <s.mass@gmail.com>
2025-07-28 17:12:14 +0200

    Test Correction : Fizzbuzz lorsqu'un nombre contient 3 et c'et une erreur.

--- exercises/4-fizzbuzz/src/fizzbuzz.py
+++ exercises/4-fizzbuzz/src/fizzbuzz.py
@@ -1,5 +1,5 @@
 def fizzbuzz(number: int) -> str:
-    if number % 3 == 0 and number % 5 == 0 or '3' in str(number):
+    if number % 3 == 0 and number % 5 == 0 :
         return "FizzBuzz"
     elif number % 3 == 0 or '3' in str(number):
         return "Fizz"
