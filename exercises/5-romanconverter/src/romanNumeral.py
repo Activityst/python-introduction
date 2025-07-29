d6ba84162991fc32bbabf5c60fef102f726213cc
dstee <s.mass@gmail.com>
2025-07-29 10:24:53 +0200

    Test nb romain

--- exercises/5-romanconverter/src/romanNumeral.py
+++ exercises/5-romanconverter/src/romanNumeral.py
@@ -6,5 +6,10 @@ def jn(n):
         100: 'C', 90: 'XC', 50: 'L', 40: 'XL',
         10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'
     }
+    roman = ''
+    for value, symbol in nb_romain.items():
+        while n >= value:
+            roman +=  symbol
+            n -= value
 
-    return 1
+    return roman
