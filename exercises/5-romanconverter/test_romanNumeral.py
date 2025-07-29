a9a306fce19f156b77de21f8175d32de1526c126
dstee <s.mass@gmail.com>
2025-07-29 10:23:22 +0200

    Test romain de 1 a 14

--- exercises/5-romanconverter/test_romanNumeral.py
+++ exercises/5-romanconverter/test_romanNumeral.py
@@ -8,3 +8,11 @@ def test_num2():
 
 def test_num4():
     assert jn(4)== "IV"
+
+def test_num5():
+    assert jn(5)== "V"
+
+def test_num9():
+    assert jn(9)== "IX"
+def test_nb14():
+    assert jn(14)== "XIV"
