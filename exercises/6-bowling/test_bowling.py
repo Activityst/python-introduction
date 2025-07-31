2cbd44024b0173adb32167f637a7ff847d2376eb
dstee <s.mass@gmail.com>
2025-07-31 12:07:54 +0200

    Test class Bowling + strike 1.0

--- exercises/6-bowling/test_bowling.py
+++ exercises/6-bowling/test_bowling.py
@@ -4,5 +4,10 @@ def test_0quilles():
     bowling0 = Bowling("-- -- -- -- -- -- -- -- -- --")
     assert bowling0.calc() == 6
 def test_bowling_part2():
-    party_2= Bowling("")
+    party_2= Bowling("35 78 66 --")
+    assert  party_2.calc()==35
+
+def test_bowling_strike():
+    strike= Bowling( " X 36 --")
+    assert  strike.calc() == 19
 
